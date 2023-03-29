707ad54847baed4f4750a6198be0d65ae7740366
marley <74526520+saltxd@users.noreply.github.com>
2023-03-29 09:53:00 -0600

    Change playRound function to use template strings

--- scripts.js
+++ scripts.js
@@ -35,34 +35,32 @@ function getComputerChoice() {
         return "scissors";
     }
 }
-console.log(getComputerChoice);
+//console.log(getComputerChoice);
 //main game function
 let playerSelection = prompt('Type: rock, paper or scissors');
 
 function playRound(playerSelection, computerSelection) {
     
     if (playerSelection === computerSelection) {
-        roundWinner = ("It\'s a tie. you both chose " + playerSelection)
-    }
-
-    if (
+        roundWinner = `It's a tie. you both chose ${playerSelection}`;
+    } else if (
        (playerSelection === 'rock' && computerSelection === 'scissors') ||
        (playerSelection === 'scissors' && computerSelection === 'paper') ||
        (playerSelection === 'paper' && computerSelection === 'rock') 
-    )    {
-        playerScore++
-        roundWinner = ("You win! " + playerSelection + " beats " + computerSelection)
-    }
-    if (
-       (playerSelection === 'rock' && computerSelection === 'paper') ||
-       (playerSelection === 'paper' && computerSelection === 'scissors') ||
-       (playerSelection === 'scissors' && computerSelection === 'rock') 
     ) {
+        playerScore++
+        roundWinner = `You win! ${playerSelection} beats ${computerSelection}`;
+    } else {
         computerScore++
-        roundWinner = ("You lose! " + computerSelection + " beats " + playerSelection)
+        roundWinner = `You lose! ${computerSelection} beats ${playerSelection}`;
     }
+ 
+}
+
+function game() {
     
 }
+
 
 function gameOver() {
     return playerScore === 5 || computerScore === 5
@@ -71,3 +69,4 @@ function gameOver() {
 //const playerSelection = 'rock';
 
 console.log(playRound(playerSelection, computerSelection));
+console.log(roundWinner);
