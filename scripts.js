30435d237e06c943eafb346f38ca8baab84eb7c5
marley <74526520+saltxd@users.noreply.github.com>
2023-04-02 08:22:01 -0600

    Deleted more old code.

--- scripts.js
+++ scripts.js
@@ -1,22 +1,4 @@
 //Rock paper scissors
-//
-//the game will play against the computer. Begin with a function
-//called getComputerChoice that returns 'Rock', 'Paper' or 'Scissors'
-//
-//write a function that plays a single round of Rock Paper Scissors
-//it should take two parameters- the playerSelection and computerSelection
-//then return a string that declares the winner: "You lose! paper beats rock"
-//- Make the playerSelection parameter case-insensitive so user can input
-//rock, ROCK, RocK
-//
-//note: you want to return the results of this function call not console.log() 
-//them. You're going to use what you return later on
-//
-//Write a new function called game(). use the previous function inside
-//of this one to play a 5 round game that keeps score and reports winner or loser
-//at the end.
-
-//global variables
 
 let playerScore = 0;
 let computerScore = 0;
